402f41271a1fb8ce9ed68aaef13cee2bccbeac87
Hattori <khattori@nii.ac.jp>
2014-05-06 22:12:18 +0900

    fix unit

---
+++ unit/Mecab.hs
@@ -0,0 +1,31 @@
+module Mecab where
+
+import Control.Monad
+import Control.Applicative
+import System.Process
+import Text.Regex.Posix
+
+mecab :: String -> IO ()
+mecab str = do
+  ls <- lines <$> readProcess "mecab" [] str
+  let ls' = merge $ map (\(s, b) -> (s, b =~ "接続詞" :: Bool)) $ map (split '\t') $ init ls
+  forM_ ls' printTag
+  
+  where
+  printTag (s, b) =
+    if b then putStrLn "<conj>" >> putStrLn s >> putStrLn "</conj>"
+         else putStrLn "<text>" >> putStrLn s >> putStrLn "</text>"
+
+split c xs = split' c xs []
+  where
+  split' c [] ac = (reverse ac, [])
+  split' c (x:xs) ac
+    | c == x     = (reverse ac, xs)
+    | otherwise  = split' c xs $ x : ac
+
+merge :: [(String, Bool)] -> [(String, Bool)]
+merge [] = []
+merge [x] = [x]
+merge ((s1, False) : (s2, False) : rest) = merge $ (s1 ++ s2, False) : rest
+merge (x : xs) = x : merge xs
+
--- unit/unit.hs
+++ unit/unit.hs
@@ -1,12 +1,11 @@
 import Control.Monad
 import Control.Applicative
-import System.Process
-import Text.Regex.Posix
 import Punc
+import Mecab
 
 main = do
   ls <- lines <$> getContents
-  loop $ splitByPunctuation ls
+  loop ls
 
   where
 
@@ -24,32 +23,5 @@ main = do
     loop $ drop 3 ls
 
   loop (x : xs) = do
-    mecab x
+    flip forM_ mecab $ punc x
     loop xs
-
-mecab :: String -> IO ()
-mecab str = do
-  ls <- lines <$> readProcess "mecab" [] str
-  let ls' = merge $ map (\(s, b) -> (s, b =~ "接続詞" :: Bool)) $ map (split '\t') $ init ls
-  forM_ ls' printTag
-  
-  where
-  printTag (s, b) =
-    if b then putStrLn "<conj>" >> putStrLn s >> putStrLn "</conj>"
-         else putStrLn "<text>" >> putStrLn s >> putStrLn "</text>"
-
-split c xs = split' c xs []
-  where
-  split' c [] ac = (reverse ac, [])
-  split' c (x:xs) ac
-    | c == x     = (reverse ac, xs)
-    | otherwise  = split' c xs $ x : ac
-
-merge :: [(String, Bool)] -> [(String, Bool)]
-merge [] = []
-merge [x] = [x]
-merge ((s1, False) : (s2, False) : rest) = merge $ (s1 ++ s2, False) : rest
-merge (x : xs) = x : merge xs
-
-splitByPunctuation :: [String] -> [String]
-splitByPunctuation = concat . map punc
