bfe22a569f89c5f063b5c77650bc000bcdffdf81
cympfh <cympfh@gmail.com>
2014-05-07 21:24:42 +0900

    emt-icon/etm-icon except `sim`

---
+++ emt-icon/emt-icon.hs
@@ -0,0 +1,27 @@
+import Data.List
+import System.IO
+import System.Environment
+import Control.Monad
+import Control.Applicative ((<$>))
+
+main = do
+  args <- getArgs
+  lex   <- readLex (args !! 0)
+  input <- lines <$> readFile (args !! 1)
+  forM_ input $ \icon -> putStrLn $ unwords $ ems lex icon
+
+readLex fname = do
+  ls <- lines <$> readFile fname
+  return $ fst $ foldl update ([], "ex") ls
+    where
+      update (ac, label) x
+        | take 2 x == "# "  = (ac, drop 2 x)
+        | otherwise         = ((x, label) : ac, label)
+
+ems :: [(String, String)] -> String -> [String]
+ems lex icon =
+  if ls == [] then ["undefined"] else ls
+  where ls = nub [ e | (i2, e) <- lex, sim icon i2]
+
+sim :: String -> String -> Bool
+sim = (==)
